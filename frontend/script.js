f56f36e66193aaafb1017d6f4af9c8d2e66df684
Alexander-Agu <theonlyagu@gmail.com>
2025-01-03 10:00:50 +0200

    Fixed the entity class and fixed the CORS bug

--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,23 @@
 const managerContainer = document.querySelector(".managerContainer");
+const saveInfo = document.querySelector(".submit");
+const names = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
 
-const getLink = 'http://localhost:8080/api/getContacts';
+const getLink = 'http://localhost:8080/contactManager/getContacts';
 
 
 
 
 const displayContacts = (data) => {
-    // let data = getData();
     return managerContainer.innerHTML += data.map(x => {
-        let {name, email, number} = x;
+        let {name, email, number, phone} = x;
         return `
                     <div class="contactContainer">
                     <div class="info">
                         <h2>${name}</h2>
                         <p>${email}</p>
-                        <p>${number}</p>
+                        <p>${phone}</p>
                     </div>
 
                     <div class="delete">
@@ -34,9 +37,49 @@ const getData = async () => {
     const data = await fetch(getLink);
 
     let tostring = await data.json();
-    // console.log(tostring)
     displayContacts(tostring)
     console.log(tostring)
-    return tostring
+
 }
 getData()
+
+const saveData = async (input) => {
+    url = 'http://localhost:8080/contactManager/saveContact';
+
+    await fetch(url, {
+        method: "POST",
+        headers: {
+        'Content-Type': 'application/json', // Matches Postman
+        },
+        body: JSON.stringify(input),
+        mode: "no-cors"
+    })
+    .then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+})
+    
+}
+
+
+const getInput = () => {
+    if (names.value && email.value && phone.value){
+        try{
+            return {
+                name: names.value,
+                email: email.value,
+                phone: phone.value
+            }
+        } catch{
+            console.log("Please enter valid information")
+        }
+    }
+    
+}
+
+saveInfo.addEventListener('click', ()=>{
+    console.log(getInput())
+    saveData(getInput())
+})
